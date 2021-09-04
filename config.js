8fcb90a696315266f0ef3bd94e29a358a43272e6
VAIBHAV JAIN <64785307+vibhu0209@users.noreply.github.com>
2021-09-04 12:35:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 import firebase from 'firebase'
 require("@firebase/firestore")
-
 const firebaseConfig = {
- //Add configuration here
- 
-};
+    apiKey: "AIzaSyDlrNlAkkJbXodbd5AGMfAY1gUAsd3M0kU",
+    authDomain: "helping-hand-complaints.firebaseapp.com",
+    projectId: "helping-hand-complaints",
+    storageBucket: "helping-hand-complaints.appspot.com",
+    messagingSenderId: "151048581460",
+    appId: "1:151048581460:web:e80daa5f981b5b2f1318c1"
+  };
   firebase.initializeApp(firebaseConfig);
 
   export default firebase.firestore()
